fd20a565dfd7b8f391eadee8211a1b554770d7da
Brian <chumobrian@gmail.com>
2024-05-03 14:07:04 +0300

    feat: Add a script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,43 @@
 #!/usr/bin/python3
-"""
-Log parsing
-"""
+"""Defines a module that reads stdin line by line and computes metrics"""
 
 import sys
+import signal
 
-if __name__ == '__main__':
 
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
+if __name__ == "__main__":
+    metrics = ""
 
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
+    def signal_handler(sig, frame):
+        """Handling keyboard interruption (CTRL + C)"""
+        global matrics
+        print(metrics)
 
-    try:
-        for line in sys.stdin:
-            count += 1
-            data = line.split()
-            try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
-                pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
-            if count % 10 == 0:
-                print_stats(stats, filesize)
-        print_stats(stats, filesize)
-    except KeyboardInterrupt:
-        print_stats(stats, filesize)
-        raise
+    def get_stats():
+        """reads stdin line by line and computes metrics"""
+        global metrics
+        lines = sys.stdin
+        timer = 10
+        file_size = 0
+        codes = {
+                   '200': 0, '301': 0, '400': 0,
+                   '401': 0, '403': 0, '404': 0,
+                   '405': 0, '500': 0
+                }
+        status_codes = codes.copy()
+        signal.signal(signal.SIGINT, signal_handler)
+        for line in lines:
+            timer -= 1
+            file_size += int(line.split(" ")[-1])
+            code = line.split(" ")[-2]
+            code_count = status_codes.get(code) + 1
+            status_codes.update({code: code_count})
+            metrics = (f"File size: {file_size}\n" +
+                       "\n".join(f"{key}: {val}"
+                                 for key, val in status_codes.items() if val))
+            if timer == 0:
+                print(metrics)
+                timer = 10
+                metrics = ""
+
+    get_stats()
