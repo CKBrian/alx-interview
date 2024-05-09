83970bd57d7277b53f94531554f9175a4aa73426
Brian <chumobrian@gmail.com>
2024-05-10 02:55:49 +0300

    Feat: Add a utf-8 validator script

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,15 +10,17 @@ def validUTF8(data: list) -> bool:
     Returns:
         bool: True if data is a valid UTF-8 encoding, else return False
     '''
+    if not data:
+        return False
     num_bytes_following = 0
     for byte in data:
         if num_bytes_following > 0:
             if byte >> 6 != 0b10:
-                return False  # Invalid continuation byte
+                return False
             num_bytes_following -= 1
         else:
             if byte >> 7 == 0:
-                continue  # Single-byte character
+                continue
             elif byte >> 5 == 0b110:
                 num_bytes_following = 1
             elif byte >> 4 == 0b1110:
@@ -26,5 +28,5 @@ def validUTF8(data: list) -> bool:
             elif byte >> 3 == 0b11110:
                 num_bytes_following = 3
             else:
-                return False  # Invalid leading byte
+                return False
     return num_bytes_following == 0
