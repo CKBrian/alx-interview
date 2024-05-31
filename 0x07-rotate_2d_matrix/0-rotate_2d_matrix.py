c65060f07837ac522a26f0e1bffa96fa707dd8f5
Brian <chumobrian@gmail.com>
2024-05-31 05:47:18 +0300

    Feat(rotate_2d_matrix): Add a function that Returns a +90 degree rotated 2D matrix.

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+"""Module for 2D Matrix rotation.
+"""
 
 
 def rotate_2d_matrix(matrix):
-    """Returns a +90 degree rotated 2D matrix.
-    Args:
-        matrix(list): A 2D list of numbers
-    Return: A new 2D list representing the rotated matrix
     """
+    Rotates a 2D matrix 90 degrees clockwise in-place.
+
+    Parameters:
+        matrix (list[list[int]]): the 2D matrix to be rotated.
+
+    Returns:
+        None
+    """
+    # First, we reverse the order of the rows
     matrix.reverse()
+    # Next, we swap the elements in the diagonal only once
     for i in range(len(matrix)):
-        for z in range(i):
-            matrix[i][z], matrix[z][i] = matrix[z][i], matrix[i][z]
+        for j in range(i):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
