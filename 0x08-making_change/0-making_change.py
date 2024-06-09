dffdbc7a99bde4545b61f522a4eae36ea6222dd6
Brian <chumobrian@gmail.com>
2024-06-09 23:38:26 +0300

    Docs(README): Add a readme file with the projects task descriptions for the 0x08-making_change project

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,26 +4,20 @@
 Makes coin change
 '''
 
+
 def makeChange(coins, total):
     '''Returns the fewest number of coins needed to make the given total'''
-    if total <= 0:
-        return 0
-
-    memo = {}
-    min_coins = findMinCoins(total, coins, memo)
-    return min_coins if min_coins != float('inf') else -1
-
     if total <= 0:
         return 0
     check = 0
     temp = 0
     coins.sort(reverse=True)
-    for i in coins:
+    for coin in coins:
         while check < total:
-            check += i
+            check += coin
             temp += 1
         if check == total:
             return temp
-        check -= i
+        check -= coin
         temp -= 1
     return -1
