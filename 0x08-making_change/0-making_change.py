3f6cce46177c6d715ad39b015bfc627719488bb4
Brian <chumobrian@gmail.com>
2024-06-09 23:33:29 +0300

    Docs(README): Add a readme file with the projects task descriptions for the 0x08-making_change project

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,29 +4,6 @@
 Makes coin change
 '''
 
-
-def findMinCoins(total, coins, memo):
-    '''Determines the minimum number of coins needed
-       to make a given total recursively'''
-    if total in memo:
-        return memo[total]
-
-    if total == 0:
-        return 0
-
-    if total < 0:
-        return float('inf')
-
-    min_coins = float('inf')
-    for coin in coins:
-        result = findMinCoins(total - coin, coins, memo)
-        if result != float('inf'):
-            min_coins = min(min_coins, result + 1)
-
-    memo[total] = min_coins
-    return min_coins
-
-
 def makeChange(coins, total):
     '''Returns the fewest number of coins needed to make the given total'''
     if total <= 0:
@@ -35,3 +12,18 @@ def makeChange(coins, total):
     memo = {}
     min_coins = findMinCoins(total, coins, memo)
     return min_coins if min_coins != float('inf') else -1
+
+    if total <= 0:
+        return 0
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
